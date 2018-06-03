c12a5c78963861ebe1efeda8015839711dce1faa
karel-burda <38255062+karel-burda@users.noreply.github.com>
2018-06-03 14:01:14 +0200

    Update warnings.cmake

--- warnings.cmake
+++ warnings.cmake
@@ -1,10 +1,20 @@
-# Sets pedantic compiler warnings in a multi-platform way
-if (MSVC)
-    if (CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
-        string(REGEX REPLACE "/W[0-4]" "/W4" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+macro(_add_pedantic_warning_level)
+    set(CXX_WARNING_FLAGS "")
+
+    if (MSVC)
+        # "/Wall" Hardly usable on MSVC without excluding stdlib
+        list(APPEND CXX_WARNING_FLAGS "/W4")
     else()
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4")
-endif()
-elseif (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-long-long -pedantic")
-endif()
+        list(APPEND CXX_WARNING_FLAGS "-Wall" "-Werror" "-pedantic" "-Wno-long-long")
+    endif()
+
+    foreach(FLAG IN LISTS CXX_WARNING_FLAGS)
+        target_compile_options(${PROJECT_NAME} PUBLIC ${FLAG})
+    endforeach()
+endmacro()
+
+macro(_supress_cxx_compiler_warning WARNING)
+    if (NOT MSVC)
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-${WARNING}")
+    endif()
+endmacro()
