fd7a397944987d1fbaedcac3820773eadd1d7bd7
Karel Burda <karel.burda@nxp.com>
2018-07-30 16:01:25 +0200

    Fixed test (updated macro signature)

--- tests/integration/gtest.test.cmake
+++ tests/integration/gtest.test.cmake
@@ -1,3 +1,3 @@
 include("${CMAKE_SOURCE_DIR}/cmake-helpers/gtest.cmake")
 
-_download_and_build_gtest("gtest-build" "Release" "gtest-src")
+_download_and_build_gtest("gtest-build" "Release" "gtest-src" "release-1.8.0")
