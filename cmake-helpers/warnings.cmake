a6e0403492f4fb8cd1ac2574c16143e8b3bf6338
Karel Burda <karel.burda@nxp.com>
2018-08-12 20:42:30 +0200

    Macros now specifying targets

--- cmake-helpers/warnings.cmake
+++ cmake-helpers/warnings.cmake
@@ -1,16 +1,15 @@
-macro(_add_pedantic_warning_level)
+macro(_add_pedantic_warning_level _TARGET)
     set(CXX_WARNING_FLAGS "")
-
     if (MSVC)
         # "/Wall" Hardly usable on MSVC without excluding stdlib
-        target_compile_options(${PROJECT_NAME} PUBLIC /W4)
+        target_compile_options(${_TARGET} PUBLIC /W4)
     else()
-        target_compile_options(${PROJECT_NAME} PUBLIC -Wall -Werror -pedantic -Wno-long-long)
+        target_compile_options(${_TARGET} PUBLIC -Wall -Werror -pedantic -Wno-long-long)
     endif()
 endmacro()
 
-macro(_supress_cxx_compiler_warning _WARNING)
+macro(_supress_cxx_compiler_warning _TARGET _WARNING)
     if (NOT MSVC)
-        target_compile_options(${PROJECT_NAME} PUBLIC -Wno-${_WARNING})
+        target_compile_options(${_TARGET} PUBLIC -Wno-${_WARNING})
     endif()
 endmacro()
