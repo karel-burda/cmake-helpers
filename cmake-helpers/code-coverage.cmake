46531a52bb2a9d4c7a30d384ed10c7fcd77e5bca
Karel Burda <karel.burda@nxp.com>
2018-09-24 11:32:33 +0200

    Added script for the code coverage macro

---
+++ cmake-helpers/code-coverage.cmake
@@ -0,0 +1,15 @@
+macro(_coverage_add_build_options _TARGET)
+    if (CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+        target_compile_options(
+            ${_TARGET}
+                PRIVATE
+                    -fprofile-arcs
+                    -ftest-coverage
+                    -fcoverage-mapping
+                    -fprofile-instr-generate)
+
+        set_target_properties(
+            ${_TARGET}
+                PROPERTIES
+                    LINK_FLAGS "-fprofile-arcs -ftest-coverage")
+endmacro()
