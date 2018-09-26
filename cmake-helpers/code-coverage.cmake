aeba7149b0318d787f048f947d6c6e092f44aa23
Karel Burda <karel.burda@nxp.com>
2018-09-26 14:13:38 +0200

    Improved macro

--- cmake-helpers/code-coverage.cmake
+++ cmake-helpers/code-coverage.cmake
@@ -1,20 +1,23 @@
 macro(_coverage_add_build_options _TARGET)
+    target_compile_options(
+        ${_TARGET}
+            PRIVATE $<$<CXX_COMPILER_ID:GNU>:
+                -fprofile-arcs
+                -ftest-coverage>)
+
+    target_compile_options(
+        ${_TARGET}
+            PRIVATE $<$<CXX_COMPILER_ID:Clang>:
+                -fprofile-arcs
+                -ftest-coverage
+                -fcoverage-mapping
+                -fprofile-instr-generate>)
+
+    # Cannot use expression generator in set_target_properties()
     if (CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU")
-        target_compile_options(
-            ${_TARGET}
-                PRIVATE
-                    -fprofile-arcs
-                    -ftest-coverage)
-
-        target_compile_options(
-            ${_TARGET}
-                PRIVATE $<$<CXX_COMPILER_ID:Clang>:
-                    -fcoverage-mapping
-                    -fprofile-instr-generate>)
-
         set_target_properties(
             ${_TARGET}
                 PROPERTIES
                     LINK_FLAGS "-fprofile-arcs -ftest-coverage")
-    endif()
+     endif()
 endmacro()
