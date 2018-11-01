66a86b7b50d0d6194a90ead23fbae41617cd0a60
Karel Burda <karel.burda@nxp.com>
2018-11-01 19:43:13 +0100

    Another clean-up

--- cmake-helpers/gtest.cmake
+++ cmake-helpers/gtest.cmake
@@ -1,25 +1,26 @@
-# Place this macro after the "add_executable" or "add_library" call
 macro(_gtest_bootstrap_and_link _target _tag)
     set(_source_dir_name "gtest-src")
-    set(_source_dir "${CMAKE_BINARY_DIR}/${_source_dir_name}")
+    set(_source_dir_path "${CMAKE_BINARY_DIR}")
 
-    add_custom_target("gtest" SOURCES "")
+    add_custom_target(gtest SOURCES "")
 
     if(NOT EXISTS "${_source_dir}")
         find_package(git REQUIRED)
 
-        # cannot do shallow clone because of later force checkout
-        execute_process(COMMAND "${GIT_EXECUTABLE} clone -b ${_tag} --depth 1 --single-branch --progress -- https://github.com/google/googletest.git ${_source_dir}"
+        execute_process(COMMAND "${GIT_EXECUTABLE} clone -b ${_tag} --depth 1 --single-branch -- https://github.com/google/googletest.git ${_source_dir_name}"
+                        WORKING_DIRECTORY ${_source_dir_path}
                         TIMEOUT 300)
     endif()
 
-    execute_process(                       COMMAND ${CMAKE_COMMAND} -DBUILD_GMOCK:BOOL=OFF -DBUILD_GTEST:BOOL=ON -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} .
-                       WORKING_DIRECTORY ${_source_dir})
-    execute_process(                       COMMAND ${CMAKE_COMMAND} --build . --config ${_GTEST_BUILD_VARIANT}
-                       WORKING_DIRECTORY ${_source_dir})
+    execute_process(COMMAND ${CMAKE_COMMAND} -DBUILD_GMOCK:BOOL=OFF -DBUILD_GTEST:BOOL=ON -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} .
+                    WORKING_DIRECTORY "${_source_dir}/${_source_dir_name}")
+    execute_process(COMMAND ${CMAKE_COMMAND} --build . --config ${_GTEST_BUILD_VARIANT}
+                    WORKING_DIRECTORY "${_source_dir}/${_source_dir_name}")
 
     #set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
-    set(GTEST_ROOT ${_source_dir})
+    #set(GTEST_ROOT ${_source_dir})
     find_package(GTest REQUIRED)
+
+    add_dependencies(${_target} gtest)
 endmacro()
--- tests/integration/run-all-integration-tests.cmake
+++
@@ -1,9 +0,0 @@
-macro(_run_integration_test test_file)
-    message(STATUS "Running integration test from ${test_file}")
-
-    include(${test_file})
-
-    message(STATUS "Done")
-endmacro()
-
-_run_integration_test("gtest.test.cmake")
