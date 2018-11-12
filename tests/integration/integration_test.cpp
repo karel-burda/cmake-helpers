b85afb6eb7073b6e64647fd6b2ecc658ab0187d7
Karel Burda <karel.burda@nxp.com>
2018-11-12 08:39:28 +0100

    Experimenting with the code coverage

--- tests/integration/integration_test.cpp
+++ tests/integration/integration_test.cpp
@@ -9,9 +9,9 @@ TEST(dummy, compilation_test)
     burda::cmake::helpers::code_coverage_test coverage_coverage;
 
     // deliberately multiple times to inspect coverage reports
-    coverage_coverage.make_foo();
-    coverage_coverage.make_foo();
-    coverage_coverage.make_foo();
+    coverage_coverage.make_foo(false);
+    coverage_coverage.make_foo(false);
+    coverage_coverage.make_foo(false);
 
     ASSERT_TRUE(true);
 }
