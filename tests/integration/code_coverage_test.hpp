b53e7998d2b187b0498de74de878f96a0fc8c420
Karel Burda <karel.burda@nxp.com>
2018-11-12 08:33:33 +0100

    Experimenting with the code coverage

--- tests/integration/code_coverage_test.hpp
+++ tests/integration/code_coverage_test.hpp
@@ -7,8 +7,13 @@ namespace cmake
 namespace helpers {
 class code_coverage_test {
     public:
-        void make_foo() {
+        void make_foo(const bool should_call_unused) {
             foo = true;
+
+            if (should_call_unused)
+            {
+                unused();
+            }
         }
 
         void unused() {
