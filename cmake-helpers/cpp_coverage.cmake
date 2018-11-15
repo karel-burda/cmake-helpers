47b9c0b9a9634e202cfcea0b125cdc573c8ac06c
Karel Burda <karel.burda@nxp.com>
2018-11-15 12:04:36 +0100

    Debugging linker flags on the CI

--- cmake-helpers/cpp_coverage.cmake
+++ cmake-helpers/cpp_coverage.cmake
@@ -10,11 +10,10 @@ macro(burda_cmake_helpers_cpp_coverage_add_build_options _target _visibility)
                     -ftest-coverage>)
 
     # At this time, cannot use target_link_options, because it'll be introduced in CMake 3.13 (currently is beta)
-    # Not using set_target_properties(LINK_FLAGS), the linker options would not be imported by oher targets
+    # Not using set_target_properties(LINK_FLAGS), since the linker options would not be imported by oher targets
     target_link_libraries(
         ${_target}
             ${_visibility}
                 $<$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>:
-                    -fprofile-arcs
-                    -ftest-coverage>)
+                    "-fprofile-arcs -ftest-coverage">)
 endmacro()
