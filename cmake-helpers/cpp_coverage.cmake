2d031d55e00fcc719dc49a05e99ae07bcc34c74d
Karel Burda <karel.burda@nxp.com>
2018-11-15 11:00:32 +0100

    Fixed coverage flags for the GCC

--- cmake-helpers/cpp_coverage.cmake
+++ cmake-helpers/cpp_coverage.cmake
@@ -6,7 +6,8 @@ macro(burda_cmake_helpers_cpp_coverage_add_build_options _target _visibility)
         ${_target}
             ${_visibility}
                 $<$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>:
-                    --coverage>)
+                    -fprofile-arcs
+                    -ftest-coverage>)
 
     # At this time, cannot use target_link_options, because it'll be introduced in CMake 3.13 (currently is beta)
     # Not using set_target_properties(LINK_FLAGS), the linker options would not be imported by oher targets
