5e4704e0efc2255ad1bc496a7acc940739e40d9c
Karel Burda <karel.burda@nxp.com>
2018-11-15 09:29:04 +0100

    Fixed settings of linker flags

--- cmake-helpers/cpp_coverage.cmake
+++ cmake-helpers/cpp_coverage.cmake
@@ -8,7 +8,9 @@ macro(burda_cmake_helpers_cpp_coverage_add_build_options _target _visibility)
                 $<$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>:
                     --coverage>)
 
-    target_links_options(
+    # At this time, cannot use target_link_options, because it'll be introduced in CMake 3.13 (currently is beta)
+    # Not using set_target_properties(LINK_FLAGS), the linker options would not be imported by oher targets
+    target_link_libraries(
         ${_target}
             ${_visibility}
                 $<$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>:
