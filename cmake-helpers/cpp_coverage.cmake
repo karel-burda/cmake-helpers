5bd2ac42f3563d94370541457be4ecf0ea71a189
Karel Burda <karel.burda@nxp.com>
2018-11-15 08:49:30 +0100

    Using target_link_options instead of dumb target_property

--- cmake-helpers/cpp_coverage.cmake
+++ cmake-helpers/cpp_coverage.cmake
@@ -6,17 +6,11 @@ macro(burda_cmake_helpers_cpp_coverage_add_build_options _target _visibility)
         ${_target}
             ${_visibility}
                 $<$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>:
-                    -fprofile-arcs
-                    -ftest-coverage>
+                    --coverage>)
 
-                $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>:
-                    -fcoverage-mapping
-                    -fprofile-instr-generate>)
-
-    if (CMAKE_CXX_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-        set_target_properties(
-            ${_target}
-                PROPERTIES
-                    LINK_FLAGS "-fprofile-arcs -ftest-coverage")
-    endif()
+    target_links_options(
+        ${_target}
+            ${_visibility}
+                $<$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>:
+                    --coverage>)
 endmacro()
