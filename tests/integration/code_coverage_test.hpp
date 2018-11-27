0c409e8d5c7740f0e6b8fa2d03bcfb9e9be2ab54
Karel Burda <karel.burda@nxp.com>
2018-11-27 05:45:56 +0100

    Improved formatting

--- tests/integration/code_coverage_test.hpp
+++ tests/integration/code_coverage_test.hpp
@@ -4,26 +4,28 @@ namespace burda
 {
 namespace cmake
 {
-namespace helpers {
-class code_coverage_test {
-    public:
-        void make_foo(const bool should_call_unused)
+namespace helpers
+{
+class code_coverage_test
+{
+public:
+    void make_foo(const bool should_call_unused)
+    {
+        foo = true;
+
+        if (should_call_unused)
         {
-            foo = true;
-
-            if (should_call_unused)
-            {
-                unused();
-            }
+            unused();
         }
+    }
 
-        void unused()
-        {
-            foo = false;
-        }
+    void unused()
+    {
+        foo = false;
+    }
 
-    private:
-        bool foo = false;
+private:
+    bool foo = false;
 };
 }
 }
