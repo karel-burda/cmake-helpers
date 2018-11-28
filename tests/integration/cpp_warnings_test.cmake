15c2108d9fed5defb317e0dab5bb069518cb2233
Karel Burda <karel.burda@nxp.com>
2018-11-28 10:24:54 +0100

    Removed useless aposthrophes

--- tests/integration/cpp_warnings_test.cmake
+++ tests/integration/cpp_warnings_test.cmake
@@ -1,4 +1,4 @@
 include(${CMAKE_CURRENT_SOURCE_DIR}/../../cmake-helpers/cpp_warnings.cmake)
 
 burda_cmake_helpers_cpp_warnings_add_pedantic_level(${PROJECT_NAME} PRIVATE)
-burda_cmake_helpers_cpp_warnings_suppress(${PROJECT_NAME} "keyword-macro" PUBLIC)
+burda_cmake_helpers_cpp_warnings_suppress(${PROJECT_NAME} keyword-macro PUBLIC)
